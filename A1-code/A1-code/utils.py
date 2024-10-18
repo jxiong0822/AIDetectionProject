6bd8f5ecc28469661d24129bd2e049a02bf500f8
jxiong32 <156553378+jxiong32@users.noreply.github.com>
2024-10-18 00:09:22 -0700

    Update utils.py

--- A1-code/A1-code/utils.py
+++ A1-code/A1-code/utils.py
@@ -55,11 +55,12 @@ class UnigramFeature(FeatureExtractor):
         index = 0
         for i in range(0, len(text_set)):
             for j in range(0, len(text_set[i])):
-                if text_set[i][j].lower() not in self.unigram:
-                    self.unigram[text_set[i][j].lower()] = index
+                if text_set[i][j].lower().strip() not in self.unigram:
+                    self.unigram[text_set[i][j].lower().strip()] = index
                     index += 1
                 else:
                     continue
+        return self.unigram
                     
     def transform(self, text: list):
         """Transform a given sentence into vectors based on the extractor you got from self.fit()
@@ -110,20 +111,72 @@ class BigramFeature(FeatureExtractor):
         raise Exception("Must be implemented")
 
 class CustomFeature(FeatureExtractor):
-    """customized feature extractor, such as TF-IDF
-    """
     def __init__(self):
-        # Add your code here!
-        raise Exception("Must be implemented")
-    def fit(self, text_set):
-        # Add your code here!
-        raise Exception("Must be implemented")
-    def transform(self, text):
-        # Add your code here!
-        raise Exception("Must be implemented")
-    def transform_list(self, text_set):
-        # Add your code here!
-        raise Exception("Must be implemented")
+        self.unigram = {}
+        self.stopwords = [
+            "a", "about", "above", "after", "again", "against", "all", "also", "am", "an", "and", "any", "are", "aren't", "as", "at",
+            "be", "because", "been", "before", "being", "below", "between", "both", "but", "by", "can't", "cannot", "could", 
+            "couldn't", "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", "each", "few", "for", 
+            "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't", "having", "he", "he'd", "he'll", "he's", 
+            "her", "here", "here's", "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", 
+            "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", "let's", "me", "more", "most", 
+            "mustn't", "my", "myself", "no", "nor", "not", "of", "off", "on", "once", "only", "or", "other", "ought", 
+            "our", "ours", "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", "she's", "should", 
+            "shouldn't", "so", "some", "such", "than", "that", "that's", "the", "their", "theirs", "them", "themselves", 
+            "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", 
+            "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", "we'd", "we'll", "we're", 
+            "we've", "were", "weren't", "what", "what's", "when", "when's", "where", "where's", "which", "while", 
+            "who", "who's", "whom", "why", "why's", "with", "won't", "would", "wouldn't", "you", "you'd", "you'll", 
+            "you're", "you've", "your", "yours", "yourself", "yourselves"
+        ]
 
+        
+    def fit(self, text_set: list):
+        """Fit a feature extractor based on given data 
+        
+        Arguments:
+            text_set {list} -- list of tokenized sentences and words are lowercased, such as [["I", "love", "nlp"], ["I", "like", "python"]]
+        """
+        index = 0
+        for i in range(0, len(text_set)):
+            for j in range(0, len(text_set[i])):
+                if text_set[i][j].lower().strip() not in self.stopwords:
+                    if text_set[i][j].lower().strip() not in self.unigram:
+                        self.unigram[text_set[i][j].lower().strip()] = index
+                        index += 1
+                    else:
+                        continue
+        return self.unigram
+                    
+    def transform(self, text: list):
+        """Transform a given sentence into vectors based on the extractor you got from self.fit()
+        
+        Arguments:
+            text {list} -- a tokenized sentence (list of words), such as ["I", "love", "nlp"]
+        
+        Returns:
+            array -- an unigram feature array, such as array([1,1,1,0,0,0])
+        """
+        feature = np.zeros(len(self.unigram))
+        for i in range(0, len(text)):
+            if text[i].lower() in self.unigram:
+                feature[self.unigram[text[i].lower()]] += 1
+        
+        return feature
+    
+    def transform_list(self, text_set: list):
+        """Transform a list of tokenized sentences into vectors based on the extractor you got from self.fit()
+        
+        Arguments:
+            text_set {list} --a list of tokenized sentences, such as [["I", "love", "nlp"], ["I", "like", "python"]]
+        
+        Returns:
+            array -- unigram feature arraies, such as array([[1,1,1,0,0], [1,0,0,1,1]])
+        """
+        features = []
+        for i in range(0, len(text_set)):
+            features.append(self.transform(text_set[i]))
+        
+        return np.array(features)
 
         
